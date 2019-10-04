f5e0f7f76b667755d08f4735e8828f36ff42558d
umaranis <mail@umaranis.com>
2019-10-04 11:20:55 +1000

    test method names should be enclosed in `` (back-tic)

--- tests/Exercism.TestRunner.FSharp.IntegrationTests/Solutions/MultipleTestsWithAllPasses/FakeTest.fs
+++ tests/Exercism.TestRunner.FSharp.IntegrationTests/Solutions/MultipleTestsWithAllPasses/FakeTest.fs
@@ -5,14 +5,14 @@ open FsUnit.Xunit
 open Fake
 
 [<Fact>]
-let Add_should_add_numbers () =
+let ``Add_should_add_numbers`` () =
     Add 1 1 |> should equal 2
 
 [<Fact(Skip = "Remove to run test")>]
-let Sub_should_subtract_numbers () =
+let ``Sub_should_subtract_numbers`` () =
     Sub 7 3 |> should equal 4
 
 [<Fact(Skip = "Remove to run test")>]
-let Mul_should_multiply_numbers () =
+let ``Mul_should_multiply_numbers`` () =
     Mul 2 3 |> should equal 6
 
