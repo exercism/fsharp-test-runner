b00fa882fcc49749ddb531a18d5004ad852093f1
umaranis <mail@umaranis.com>
2019-10-03 17:31:53 -0700

    MultipleTestsWithAllPasses project ported from C# to F#

---
+++ tests/Exercism.TestRunner.FSharp.IntegrationTests/Solutions/MultipleTestsWithAllPasses/FakeTest.fs
@@ -0,0 +1,18 @@
+module FakeTest
+
+open Xunit
+open FsUnit.Xunit
+open Fake
+
+[<Fact>]
+let Add_should_add_numbers () =
+    Add 1 1 |> should equal 2
+
+[<Fact(Skip = "Remove to run test")>]
+let Sub_should_subtract_numbers () =
+    Sub 7 3 |> should equal 4
+
+[<Fact(Skip = "Remove to run test")>]
+let Mul_should_multiply_numbers () =
+    Mul 2 3 |> should equal 6
+
