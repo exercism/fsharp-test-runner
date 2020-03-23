fe9409b487f51eab92565708293d5a66d56bee04
Erik Schierboom <erik_schierboom@hotmail.com>
2020-03-23 15:58:56 +0100

    Update expected test output

---
+++ tests/Exercism.TestRunner.FSharp.IntegrationTests/Solutions/MultipleTestsWithTestOutputExceedingLimit/FakeTests.fs
@@ -0,0 +1,14 @@
+module FakeTest
+
+open Xunit
+open FsUnit.Xunit
+open Fake
+
+[<Fact>]
+let ``Add should add numbers``() = add 1 1 |> should equal 2
+
+[<Fact>]
+let ``Sub should subtract numbers``() = sub 7 3 |> should equal 4
+
+[<Fact>]
+let ``Mul should multiply numbers``() = mul 2 3 |> should equal 6
