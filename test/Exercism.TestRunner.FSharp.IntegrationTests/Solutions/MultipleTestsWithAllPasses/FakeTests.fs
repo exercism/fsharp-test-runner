562b60b759c540facfd9c622339cc603eae4fe94
Erik Schierboom <erik_schierboom@hotmail.com>
2021-04-11 19:49:48 +0200

    Add test for different test code formats

--- test/Exercism.TestRunner.FSharp.IntegrationTests/Solutions/MultipleTestsWithAllPasses/FakeTests.fs
+++ test/Exercism.TestRunner.FSharp.IntegrationTests/Solutions/MultipleTestsWithAllPasses/FakeTests.fs
@@ -4,7 +4,9 @@ open Xunit
 open FsUnit.Xunit
 open Fake
 
+[<Fact>]
 let ``Add should add numbers`` () = add 1 1 |> should equal 2
+
 [<Fact>]
 let ``Sub should subtract numbers`` () = sub 7 3 |> should equal 4
 
