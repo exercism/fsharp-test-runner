5e500c64f863dafd70a956a85fdb9654485e9fc4
Erik Schierboom <erik_schierboom@hotmail.com>
2021-04-11 19:09:23 +0200

    Fix failing test

--- test/Exercism.TestRunner.FSharp.IntegrationTests/Solutions/MultipleTestsWithAllPasses/FakeTests.fs
+++ test/Exercism.TestRunner.FSharp.IntegrationTests/Solutions/MultipleTestsWithAllPasses/FakeTests.fs
@@ -4,25 +4,11 @@ open Xunit
 open FsUnit.Xunit
 open Fake
 
-type Tests(testOutput: Xunit.Abstractions.ITestOutputHelper) =
-    class
-        [<Fact>]
-        let ``Add should add numbers`` () = add 1 1 |> should equal 2
+[<Fact>]
+let ``Add should add numbers`` () = add 1 1 |> should equal 2
 
-        [<Fact>]
-        let ``Sub should subtract numbers`` () = sub 7 3 |> should equal 4
+[<Fact>]
+let ``Sub should subtract numbers`` () = sub 7 3 |> should equal 4
 
-        [<Fact>]
-        let ``Mul should multiply numbers`` () = mul 2 3 |> should equal 6
-
-        let stringWriter = new System.IO.StringWriter()
-        do
-            System.Console.SetOut(stringWriter)
-            System.Console.SetError(stringWriter)
-            System.Diagnostics.Trace.Listeners.Add(new System.Diagnostics.ConsoleTraceListener())
-            |> ignore
-
-        interface System.IDisposable with
-            override __.Dispose() =
-                testOutput.WriteLine(stringWriter.ToString())
-    end
+[<Fact>]
+let ``Mul should multiply numbers`` () = mul 2 3 |> should equal 6
