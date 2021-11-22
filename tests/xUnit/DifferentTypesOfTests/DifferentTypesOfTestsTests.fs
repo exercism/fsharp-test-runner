ac813324ce03138148b9be1842d5d62b62132fa1
Erik Schierboom <erik_schierboom@hotmail.com>
2021-11-22 08:20:33 +0100

    Revert

--- tests/xUnit/DifferentTypesOfTests/DifferentTypesOfTestsTests.fs
+++ tests/xUnit/DifferentTypesOfTests/DifferentTypesOfTestsTests.fs
@@ -15,20 +15,20 @@ type CustomPropertyAttribute() =
 [<Fact>]
 let ``Add should add numbers`` () = add 1 1 |> should equal 2
 
-[<Fact>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Add should add more numbers`` () = add 2 3 |> should equal 5
 
-[<Fact(Timeout = 20)>]
-let ``Add should add more numbers with timeout`` () : Task =
+[<Fact(Timeout = 20, Skip = "Remove this Skip property to run this test")>]
+let ``Add should add more numbers with timeout`` (): Task =
     Task.Delay(TimeSpan.FromMilliseconds(100.0))
 
-[<Theory>]
+[<Theory(Skip = "Remove this Skip property to run this test")>]
 [<InlineData(4, 7, 3)>]
 let ``Sub should subtract numbers`` (expected, x, y) = sub x y |> should equal expected
 
-[<CustomPropertyAttribute>]
+[<CustomPropertyAttribute(Skip = "Remove this Skip property to run this test")>]
 let ``Mul should multiply numbers`` (x, y) = mul x y |> should equal (x * y)
 
-[<Property>]
+[<Property(Skip = "Remove this Skip property to run this test")>]
 let ``Div should divide numbers`` (x) : Property =
     Prop.throws<DivideByZeroException, int> (new Lazy<int>(fun () -> x / 0))
