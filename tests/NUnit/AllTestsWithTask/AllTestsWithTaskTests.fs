dae4ff893f46e929e60e55dac6cd480c2547c1de
Erik Schierboom <erik_schierboom@hotmail.com>
2021-11-22 08:11:04 +0100

    Disable forward reference warning

--- tests/NUnit/AllTestsWithTask/AllTestsWithTaskTests.fs
+++ tests/NUnit/AllTestsWithTask/AllTestsWithTaskTests.fs
@@ -10,11 +10,11 @@ open AllTestsWithTask
 let ``Add should add numbers`` () = add 1 1 |> should equal 2
 
 [<Test>]
-[<Ignore("Remove this Attribute to run this test")>]
+
 [<Task(2)>]
 let ``Sub should subtract numbers`` () = sub 7 3 |> should equal 4
 
 [<Test>]
-[<Ignore("Remove this Attribute to run this test")>]
+
 [<Task(3)>]
 let ``Mul should multiply numbers`` () = mul 2 3 |> should equal 6
